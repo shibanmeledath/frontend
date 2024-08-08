c75262997ec990136f9ab623e38411ee55fc2728
Shiban <2000.shiban@gmail.com>
2024-08-08 19:35:31 +0530

    test error corection

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 // src/App.test.js
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
